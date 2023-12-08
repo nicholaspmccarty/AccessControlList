a2d6a525ad0976c4b4f43d962cfc42ffda286310
mccartnp <nicholasmccarty252@gmail.com>
2023-12-08 15:17:09 -0500

    created addUser() function

--- acl.cpp
+++ acl.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 
 
-struct user {
+struct User {
     std::string username;
     std::string password;
 };
 
-struct file {
+struct File {
     std::string file;
     std::string fileContent;
 
 };
 
 struct ACL {
+  std::unordered_map<std::string, std::string> userPermissions;
 
 };
 
+void addUser(User& user, const std::string& userName, const std::string& passWord) {
+    user.username = userName;
+    user.password = passWord;
+}
+
 
 int main() {
+    
     return -1;
 }
