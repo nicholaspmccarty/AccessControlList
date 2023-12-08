73cf1fadeef602a7fee0a3e506be1bd0391a3b37
mccartnp <nicholasmccarty252@gmail.com>
2023-12-08 15:06:28 -0500

    first commit with README

---
+++ acl.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+
+
+struct user {
+    std::string username;
+    std::string password;
+};
+
+struct file {
+    std::string file;
+    std::string fileContent;
+
+};
+
+struct ACL {
+
+};
+
+
+int main() {
+    return -1;
+}
