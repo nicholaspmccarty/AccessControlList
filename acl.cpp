a3a08a4c2bc056992aec371c3d430db3f7c9a9c2
mccartnp <nicholasmccarty252@gmail.com>
2023-12-08 15:32:59 -0500

    added remove_user

--- acl.cpp
+++ acl.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 
 
@@ -20,13 +21,41 @@ struct ACL {
 
 };
 
-void addUser(User& user, const std::string& userName, const std::string& passWord) {
-    user.username = userName;
-    user.password = passWord;
+void add_user(std::vector<User>& users, const std::string& name, const std::string& password) {
+    users.push_back({name, password});
 }
 
+void remove_user(std::vector<User>& users, const std::string& name, const std::string& password) {
+    int counter = 0;
+    for (User& k : users) {
+        if (k.username == name && k.password == password) {
+            users.erase(users.begin() + counter);
+            break; // exit the loop after removing the user
+        }
+        counter++;
+    }
+}
+
+void displayUsers(std::vector<User> users) {
+    for (User k : users) {
+        std::cout << k.username << std::endl;
+    }
+}
 
 int main() {
-    
+    std::vector<User> users;
+    std::vector<File> files;
+    // Testing add_user works properly. 
+    add_user(users, "root", "hellothere");
+    add_user(users, "user1", "password");
+    add_user(users, "user2", "secret");
+    add_user(users, "user3", "dolphin");
+    displayUsers(users);
+    remove_user(users, "root", "hellothere");
+    displayUsers(users);
+
+
+
+
     return -1;
 }
